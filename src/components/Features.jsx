bc9f58423830d3df4bfcb1a461acb20b3a956397
Ali <ali.esmaeili.dev@gmail.com>
2026-07-10 00:34:51 +0330

    initializing feature section and other sections

---
+++ src/components/Features.jsx
@@ -0,0 +1,92 @@
+import { Canvas } from "@react-three/fiber";
+import { Suspense, useEffect, useRef } from "react";
+import StudioLights from "./three/StudioLights";
+import { features, featureSequence } from "../constants";
+import clsx from "clsx";
+import { MacbookModel } from "./models/Macbook";
+import { useMediaQuery } from "react-responsive";
+import { Html } from "@react-three/drei";
+import useMacbookStore from "../store";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const ModelScroll = () => {
+  const isMobile = useMediaQuery({ query: "(max-width:1024px)" });
+  const groupRef = useRef(null);
+  const { setTexture } = useMacbookStore();
+  useEffect(() => {
+    featureSequence.forEach((feature) => {
+      const video = document.createElement("video");
+      Object.assign(video, {
+        src: feature.videoPath,
+        muted: true,
+        playsInline: true,
+        preload: "auto",
+        crossOrigin: "anonymous",
+      });
+      video.load();
+    });
+  }, []);
+  useGSAP(() => {
+    const modelTimeLine = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#f-canvas",
+        start: "top top",
+        end: "bottom top",
+        scrub: 1,
+        pin: true,
+      },
+    });
+    const timeLine = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#f-canvas",
+        start: "top center",
+        end: "bottom top",
+        scrub: 1,
+      },
+    });
+    if (groupRef.current) {
+      modelTimeLine.to(groupRef.current.rotation, {
+        y: Math.PI * 2,
+        ease: "power1.inOut",
+      });
+    }
+  });
+  return (
+    <group ref={groupRef}>
+      <Suspense
+        fallback={
+          <Html>
+            <h1 className="text-white text-3xl uppercase">Loading...</h1>
+          </Html>
+        }
+      >
+        <MacbookModel scale={isMobile ? 0.05 : 0.08} position={[0, -1, 0]} />
+      </Suspense>
+    </group>
+  );
+};
+const Features = () => {
+  return (
+    <section id="features">
+      <h2>See it all in a new light.</h2>
+      <Canvas id="f-canvas" camera={{}}>
+        <StudioLights />
+        <ambientLight intensity={0.5} />
+        <ModelScroll />
+      </Canvas>
+      <div className="absolute inset-0">
+        {features.map((feature, index) => (
+          <div
+            className={clsx(`box`, `box${index + 1}`, feature.styles)}
+            key={index}
+          >
+            {feature.text}
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Features;
